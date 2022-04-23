3a732f5d0271921df4cce9f6308a01c2a73a24cc
Henmar13 <you@example.com>
2022-04-22 22:52:35 -0300

    cambios

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+<footer class="container mt-5">
+        <div class="row">
+            <div class="col-12 col-sm-3">
+                <a href="https://github.com" target="_blank">
+                    <i class="fa-brands fa-github"></i></a>
+                <a href="https://linkedin.com" target="_blank">
+                    <i class="fa-brands fa-linkedin"></i></a>
+            </div>
+            <div class="col-12 col-sm-3">Sponsor<a href="http://depsuite.com">DePC Suite</a></div>
+            <div class="col-12 col-sm-3"><a href="mailto:henmarmedina@gmail.com">henmarmedina@gmail.com.ar</a></div>
+            <div class="col-12 col-sm-3"></div>
+    </footer>
